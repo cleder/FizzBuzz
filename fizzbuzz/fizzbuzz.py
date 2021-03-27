2f54a244a6dfb1f86f402b90db6b416a451dc850
Christian Ledermann <christian.ledermann@blueface.com>
2021-03-27 23:11:25 +0000

    refactor, verify coverage with mutmut, add type annotations

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""A generic configurable FizzBuzz."""
+
+from functools import partial
+from typing import Callable, Optional, Tuple, cast
+
+
+def byn(number: int, div: int, out: str) -> str:
+    assert isinstance(number, int)
+    assert isinstance(div, int)
+    if number % div == 0:
+        return out
+    return ""
+
+
+callback = Callable[[int], str]
+
+
+class FizzBuzz(object):
+
+    _actions: Tuple[callback, ...] = (
+        cast(callback, partial(byn, div=3, out="fizz")),
+        cast(callback, partial(byn, div=5, out="buzz")),
+    )
+
+    def __init__(
+        self,
+        actions: Optional[Tuple[callback, ...]] = None,
+        default_action: Optional[callback] = None,
+    ) -> None:
+        if actions:
+            self._actions = actions
+        if default_action:
+            self._default_action = default_action
+        else:
+            self._default_action = cast(callback, str)
+
+    def response(self, number: int) -> str:
+        rstr = "".join(action(number) for action in self._actions)
+        if not rstr:
+            rstr = self._default_action(number)
+        return rstr
